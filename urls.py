48cc24cfebae49607f097e9804e6a71eaf1f901a
BuildTools <unconfigured@null.spigotmc.org>
2025-03-10 13:19:29 +0900

    Flatten directory structure for Render

--- urls.py
+++ urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from django.contrib import admin
-from django.views.generic import TemplateView  # 仮にテンプレートを表示
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
