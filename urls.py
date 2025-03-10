1c4b90afd6da0f38ac5e5ecab0e56d3993d2a447
BuildTools <unconfigured@null.spigotmc.org>
2025-03-10 13:22:18 +0900

    Add import for django.contrib.admin in urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib import admin  # 追加
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
